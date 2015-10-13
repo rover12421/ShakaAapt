24615ae495e087c5bb389781f5dbff6fdeb62a0a
Rover12421 <rover12421@163.com>
2015-10-13 16:08:57 +0800

    修改OSX的编译脚本

--- build_ShakaAapt_all_in_maxos.sh
+++ build_ShakaAapt_all_in_maxos.sh
@@ -14,15 +14,17 @@ lunch sdk-eng
 
 OUT_DIR=out-x86 make -B BUILD_NUMBER=$BUILD_NUMBER LOCAL_MULTILIB=32 aapt -j4
 mkdir -p "$BinDir/darwin-x86/bin"
-mkdir -p lib "$BinDir/darwin-x86/lib"
 cp out-x86/host/darwin-x86/bin/aapt "$BinDir/darwin-x86/bin/aapt"
-cp out-x86/host/darwin-x86/lib/libc++.dylib "$BinDir/darwin-x86/lib/libc++.dylib"
+
+#mkdir -p lib "$BinDir/darwin-x86/lib"
+#cp out-x86/host/darwin-x86/lib/libc++.dylib "$BinDir/darwin-x86/lib/libc++.dylib"
 
 # darwin-x86_64
 
 OUT_DIR=out-x86_64 make -B BUILD_NUMBER=$BUILD_NUMBER LOCAL_MULTILIB=64 aapt -j4
 mkdir -p "$BinDir/darwin-x86_64/bin"
-mkdir -p "$BinDir/darwin-x86_64/lib64/"
 cp out-x86_64/host/darwin-x86/bin/aapt "$BinDir/darwin-x86_64/bin/aapt"
-cp out-x86_64/host/darwin-x86/lib64/libc++.dylib "$BinDir/darwin-x86_64/lib64/libc++.dylib"
+
+#mkdir -p "$BinDir/darwin-x86_64/lib64/"
+#cp out-x86_64/host/darwin-x86/lib64/libc++.dylib "$BinDir/darwin-x86_64/lib64/libc++.dylib"
 
