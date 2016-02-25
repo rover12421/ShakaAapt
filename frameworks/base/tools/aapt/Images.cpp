11db3a738abe40d0ca1f9f9445af7482cf9be96d
Rover12421 <rover12421@163.com>
2016-02-25 18:12:49 +0800

    Modify Error to Warn from `Failure processing PNG image`

--- frameworks/base/tools/aapt/Images.cpp
+++ frameworks/base/tools/aapt/Images.cpp
@@ -1362,6 +1362,13 @@ bail:
     if (write_ptr) {
         png_destroy_write_struct(&write_ptr, &write_info);
     }
+    //[Rover12421]>
+    if (error != NO_ERROR) {
+        fprintf(stdout, "WARIN: Failure processing PNG image %s\n",
+                file->getPrintableSource().string());
+    }
+    return NO_ERROR;
+    //[Rover12421]<
 
     if (error != NO_ERROR) {
         fprintf(stderr, "ERROR: Failure processing PNG image %s\n",
